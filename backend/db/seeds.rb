7dccfc0ef6cd8d549249b0b339763144faaf32de
ikedakohei <kouhik06@gmail.com>
2021-07-20 19:50:07 +0900

    seedで初期データを登録できるようにする

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.create!(name: '予定1', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定2', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定3', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定4', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
