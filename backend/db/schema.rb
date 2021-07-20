7cfad8797f25c1dff633b25eb2555ac527ea1629
ikedakohei <kouhik06@gmail.com>
2021-07-20 19:45:21 +0900

    Eventモデルを作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_20_104149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.datetime "start", null: false
+    t.datetime "end", null: false
+    t.boolean "timed", default: true
+    t.text "description"
+    t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
