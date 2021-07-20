640f0334cbd9261747f77c9a6dea731333e199db
ikedakohei <kouhik06@gmail.com>
2021-07-20 20:00:34 +0900

    イベント詳細取得APIを作成する

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -2,4 +2,8 @@ class EventsController < ApplicationController
   def index
     render json: Event.all
   end
+
+  def show
+    render json: Event.find(params[:id])
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :events, only: %i[index]
+  resources :events, only: %i[index show]
 end
