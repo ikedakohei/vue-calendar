06617ca5be749d01b017f90aca2f22139ddd2851
ikedakohei <kouhik06@gmail.com>
2021-07-21 21:19:48 +0900

    Vuexを使って状態管理を行う

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
     <h1>Calendar</h1>
-    <p>events:</p>
-    <p>{{ events }}</p>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails />
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { mapGetters, mapActions } from 'vuex';
+import CalendarDetails from './CalendarDetails.vue';
 
 export default {
   name: 'Calendar',
-  data: () => ({
-    events: [],
-  }),
-  methods: {
-    fetchEvents() {
-      axios
-        .get('http://localhost:3000/events')
-        .then((response) => {
-          this.events = response.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
+  components: {
+    CalendarDetails,
   },
+  computed: {
+    ...mapGetters('events', ['events']),
+  },
+  methods: {
+    ...mapActions('events', ['fetchEvents'])
+  }
 };
 </script>
